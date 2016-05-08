d3e861a8e6cb400c00a3bcf5dc6147a8570d43f5
Elizabeth Maraan <emaraan@gmail.com>
2016-05-07 23:20:25 -0700

    Moved files up one folder and Completed intro to bootstrap grids

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: 'mongodb://emaraan:Onetw00@ds034279.mlab.com:34279/mean-database',
     options: {
       user: '',
       pass: ''
