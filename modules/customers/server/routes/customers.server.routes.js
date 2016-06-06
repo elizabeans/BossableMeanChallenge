99d4f3daae2a7cc196130aa0de867abf24ea4e27
Elizabeth Maraan <emaraan@gmail.com>
2016-06-05 22:57:50 -0700

    Updated Customers Server Routes

--- modules/customers/server/routes/customers.server.routes.js
+++ modules/customers/server/routes/customers.server.routes.js
@@ -1,21 +1,18 @@
 'use strict';
 
-/**
- * Module dependencies
- */
-var customersPolicy = require('../policies/customers.server.policy'),
-  customers = require('../controllers/customers.server.controller');
-
 module.exports = function(app) {
-  // Customers Routes
-  app.route('/api/customers').all(customersPolicy.isAllowed)
-    .get(customers.list)
-    .post(customers.create);
+  var users = require('../../app/controllers/users');
+  var customers = require('../../app/controllers/customers');
 
-  app.route('/api/customers/:customerId').all(customersPolicy.isAllowed)
+  // Customers Routes
+  app.route('/customers')
+    .get(customers.list)
+    .post(users.requiresLogin, customers.create);
+
+  app.route('/customers/:customerId')
     .get(customers.read)
-    .put(customers.update)
-    .delete(customers.delete);
+    .put(users.requiresLogin, customers.hasAuthorization, customers.update)
+    .delete(users.requiresLogin, customers.hasAuthorization, customers.delete);
 
   // Finish by binding the Customer middleware
   app.param('customerId', customers.customerByID);
